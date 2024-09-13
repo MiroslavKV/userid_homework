90db77823a39c4f27c6e094002ffe8b954438618
MiroslavKV <miroslavkolesnykov@gmail.com>
2024-09-13 18:40:03 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-from http.client import HTTPException
 from fastapi import FastAPI
-from fastapi import status
+from fastapi import status, HTTPException, Query
 import uvicorn
 
 app = FastAPI(debug=True)
 
-users = {
-    "Myroslav": {"age": 13, "work": "student"},
-    "name": {"age": 14, "work": "nothing"}
-}
+users = [
+    {"name": "Myroslav", "age": 13, "work": "student"},
+    {"name": "name", "age": 14, "work": "nothing"}
+]
 
 @app.get("/users")
 def get_users():
@@ -16,25 +15,29 @@ def get_users():
 
 @app.post("/users", status_code=status.HTTP_201_CREATED)
 def add_user(name: str, age: int, work: str):
-    if name in users:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"User {name} already exists")
+    users_name = [i["name"] for i in users]
+    if name in users_name:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"User {name} is exists")
 
-    users[name] = {"age": age, "work": work}
+    users.append({"name": name, "age": age, "work": work})
     return {"name": name, "age": age, "work": work}
 
-@app.get("/users/{name}")
-def user_by_name(name: str):
-    if name not in users:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"User {name} does not exist")
-    return {name: users[name]}
+
+@app.get("/users/{id_}")
+def user_by_id(id_: int):
+    if id_ > len(users) - 1:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"User not exists")
+    return users[id_]
+
 
 @app.delete("/users", status_code=status.HTTP_204_NO_CONTENT)
 def delete_user(name: str):
-    if name not in users:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"User {name} does not exist")
-
-    del users[name]
-    return {"message": f"User {name} deleted"}
+    users_name = [i["name"] for i in users]
+    if name not in users_name:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"User {name} not exists")
+    id_ = users_name.index(name)
+    users.pop(id_)
+    return {"mess": f"{name} deleted"}
 
 if __name__ == "__main__":
     uvicorn.run(f"{__name__}:app", reload=True)
